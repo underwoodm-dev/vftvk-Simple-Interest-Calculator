f4e4bc9d3b1f37d2d7bba016b5f51f1a90a1bd94
underwoodm-dev <83571955+underwoodm-dev@users.noreply.github.com>
2021-08-17 18:36:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,5 @@
-
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate / 100;
-    var year = new Date().getFullYear()+parseInt(years);
-    
-    var result = document.getElementById("result").value;
-    
-    p = document.getElementById("principal").value;
-    
     function updateRate() 
     {
         var rateval = document.getElementById("rate").value;
-        document.getElementById("rate_val").innerText=rateval;
+        document.getElementById("rate_val").innerText=rateval + "%";
     }
-    
-    function computeInterest()
-    {
-        document.getElementById("result").innerHTML="If you deposit <mark> "+principal+",\ <br\> </mark> at an interest rate of <mark> "+rate+"%\<br\> </mark>You will receive an amount of <mark> "+amount+",\<br\> </mark> in the year <mark> "+year+"\<br\>" </mark>
-    }
-    
-    function validate()
-    {
-        if (principal <= 0) {
-            alert("Enter a Positive Number);
-        }
-    }
-        
-    function resetForm()
-        {
-            document.getElementById("principal").focus();
-        }
-
-        
