c33b419fc8e7de37c7715dfcc8aaa253bbb6d763
underwoodm-dev <83571955+underwoodm-dev@users.noreply.github.com>
2021-08-17 18:46:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-    function updateRate() 
-    {
-        var rateval = document.getElementById("rate").value;
-        document.getElementById("rate_val").innerText=rateval + "%";
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        principalField.focus();
+        return;
     }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    
+    var interest = principal * years * rate / 100;
+
+    var year = new Date()
+    var this_year = now.getFullYear() + Number(years);
+
+    document.getElementById("result").innerHTML = 
+        "<div>" +
+        "If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "%</mark>. <br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/>" +
+        "in the year <mark>" + this_year + "</mark>." +
+        "</div>";        
+}    
+
+function updateRate 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval + "%";
+}
